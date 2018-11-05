5f687561ee02daa96b52b70fd631b367b7195876
github username <github email address>
2018-11-05 14:34:34 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+	before_action :fetch_post, only: [:update, :show, :edit]
+
 	def index
 		@posts = Post.all
 	end
 
 	def show
-		@post = Post.find(params[:id])
 	end
 
 	def new
@@ -12,18 +13,27 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params["post"])
+	  @post = Post.new(post_params(:title,:description))
 	  @post.save
 	  redirect_to post_path(@post)
 	end
 
 	def update
-	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
+	  # @post = Post.find(params[:id])
+		@post.update(post_params(:title))
 	  redirect_to post_path(@post)
 	end
 
 	def edit
-	  @post = Post.find(params[:id])
+	end
+
+	private
+
+	def fetch_post
+		@post = Post.find(params[:id])
+	end
+
+	def post_params(*args)
+		params.require(:post).permit(*args)
 	end
 end
